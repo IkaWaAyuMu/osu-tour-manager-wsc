b952688a4f20ef946622b1fe7973e325431555f3
IkaWaAyuMu <pungpond4450@gmail.com>
2022-02-07 13:21:07 +0700

    Use "setMatchIndex" on set match.

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ type setNumberFunction = (arg: number) => void;
 export default function App() {
 
     const [fetchedData, setFetchedData] = useState<TourData[]>([]);
-    const [roundSelect, setRoundSelect] = useState(-1);
+    const [roundSelect, setRoundSelect] = useState(0);
 
     useEffect((): any => {
 
@@ -31,9 +31,8 @@ export default function App() {
         socket.onmessage = (e) => {
             console.log(`Recieved: ${e.data}`);
             const temp: OsuTourManagerWebSocketServerSendMessage = JSON.parse(e.data);
-            if (temp.message === "fetchTourData" && temp.status === 0) socket.sendStrictMessage({ message: "getTourData" })
             if (temp.message === "getTourData" && temp.status === 0 && temp.tourData !== undefined) setFetchedData(temp.tourData);
-            if (temp.message === "setMatch") socket.sendStrictMessage({ message: "getMatch" });
+            if (temp.message === "setMatchIndex") socket.sendStrictMessage({ message: "getMatchIndex" });
         }
     });
 
@@ -49,7 +48,8 @@ export default function App() {
                         { /* Setup */}
                         <Route path="/" element={
                             <div>
-                                <button style={{ margin: "10px 80px 20px 80px", width: "200px", borderRadius: "5px", fontSize: "25px", backgroundColor: "#8F8F8FFF", color: "white" }} onClick={() => socket.sendStrictMessage({ message: "fetchTourData" })}>refetch</button>
+                                <button style={{ margin: "10px 80px 5px 80px", width: "200px", borderRadius: "5px", fontSize: "25px", backgroundColor: "#8F8F8FFF", color: "white" }} onClick={() => socket.sendStrictMessage({ message: "fetchTourData" })}>refetch</button>
+                                <button style={{ margin: "5px 80px 10px 80px", width: "200px", borderRadius: "5px", fontSize: "25px", backgroundColor: "#8F8F8FFF", color: "white" }} onClick={() => socket.sendStrictMessage({ message: "getTourData" })}>apply fetched</button>
                                 <div style={{ margin: "10px 10px 10px 10px", width: "90%", display: "flex", alignItems: "center", justifyContent: "flex-end" }}>
                                     <div style={{ color: "white" }}>Round</div>
                                     <div>|||||</div>
@@ -60,7 +60,7 @@ export default function App() {
                                     <div>|||||</div>
                                     {<MatchSelect fetchedData={fetchedData} index={roundSelect}/>}
                                 </div>
-                                <button style={{ margin: "10px 80px 10px 80px", width: "200px", borderRadius: "5px", fontSize: "25px", backgroundColor: "#8F8F8FFF", color: "white" }} onClick={() => socket.sendStrictMessage({ message: "setMatch", match: Matchvalue(fetchedData)})}>set match</button>
+                                <button style={{ margin: "10px 80px 10px 80px", width: "200px", borderRadius: "5px", fontSize: "25px", backgroundColor: "#8F8F8FFF", color: "white" }} onClick={() => socket.sendStrictMessage({ message: "setMatchIndex", matchIndex: Matchvalue(fetchedData)})}>set match</button>
                             </div>
                         } />
                         { /* Draft */}
@@ -81,30 +81,33 @@ function RoundSelect(props :{fetchedData: TourData[], setIndexFunction: setNumbe
     function setIndex(): void {
         const select = document.getElementsByName("round")[0] as HTMLSelectElement;
         if (select === undefined) return;
-        setIndexFunction(select.selectedIndex);
+        setIndexFunction(select.selectedIndex-2);
     }
-    if (fetchedData.length <= 0) return (<select name="round" style={{ width: "200px", borderRadius: "5px", fontSize: "25px", backgroundColor: "#8F8F8FFF", color: "white" }}></select>);
+    if (fetchedData.length <= 0) return (<select name="round" style={{ width: "200px", borderRadius: "5px", fontSize: "25px", backgroundColor: "#8F8F8FFF", color: "white" }}><option key={count++} value={-1}>Please apply fetch</option></select>);
     const roundOptions: JSX.Element[] = [];
-    fetchedData.map((tourData => {
-        roundOptions.push(<option key={count++} value={tourData.round}>{tourData.round}</option>);
+    fetchedData.map(((tourData, index) => {
+        roundOptions.push(<option key={count++} value={index}>{tourData.round}</option>);
     }))
-    return (<select name="round" style={{ width: "200px", borderRadius: "5px", fontSize: "25px", backgroundColor: "#8F8F8FFF", color: "white"}} onChange={setIndex}>{roundOptions}</select>);
+    return (<select name="round" style={{ width: "200px", borderRadius: "5px", fontSize: "25px", backgroundColor: "#8F8F8FFF", color: "white"}} onChange={setIndex} ><option key={count++} value={-1}>Please select</option><option disabled>──────────</option>{roundOptions}</select>);
 }
 
 function MatchSelect(props :{fetchedData: TourData[], index: number}) {
     const {fetchedData, index} = props;
-    if (fetchedData.length <= 0) return (<select name="match" style={{ width: "200px", borderRadius: "5px", fontSize: "25px", backgroundColor: "#8F8F8FFF", color: "white" }}></select>);
+    if (fetchedData.length <= 0) return (<select name="match" style={{ width: "200px", borderRadius: "5px", fontSize: "25px", backgroundColor: "#8F8F8FFF", color: "white" }}><option key={count++} value={-1}>Please apply fetch</option></select>);
     const matchOptions: JSX.Element[] = [];
     if (index < 0) return (<select name="match" style={{ width: "200px", borderRadius: "5px", fontSize: "25px", backgroundColor: "#8F8F8FFF", color: "white" }}></select>);
-    fetchedData[index].matches.map((match) => {
-        matchOptions.push(<option key={count++} value={match.match}>{match.match}</option>);
+    fetchedData[index].matches.map((match, index) => {
+        matchOptions.push(<option key={count++} value={index}>{match.match}</option>);
     })
-    return (<select name="match" style={{ width: "200px", borderRadius: "5px", fontSize: "25px", backgroundColor: "#8F8F8FFF", color: "white" }}>{matchOptions}</select>);
+    return (<select name="match" style={{ width: "200px", borderRadius: "5px", fontSize: "25px", backgroundColor: "#8F8F8FFF", color: "white" }}><option key={count++} value={-1}>Please select</option><option disabled>──────────</option>{matchOptions}</select>);
 }
 
 function Matchvalue(tourData: TourData[]) {
-    if (tourData[(document.getElementsByName("round")[0] as HTMLSelectElement).selectedIndex] === undefined) return {round: "" ,match: ""};
-    const round = tourData[(document.getElementsByName("round")[0] as HTMLSelectElement).selectedIndex]
-    if (round.matches[(document.getElementsByName("match")[0] as HTMLSelectElement).selectedIndex] === undefined) return {round: round.round, match: ""};
-    return {round: round.round, match: round.matches[(document.getElementsByName("match")[0] as HTMLSelectElement).selectedIndex].match};
+    if (parseInt((document.getElementsByName("round")[0] as HTMLSelectElement).value) === undefined) return {round: -1 ,match: -1};
+    const roundIndex = parseInt((document.getElementsByName("round")[0] as HTMLSelectElement).value);
+    if (roundIndex < 0 || roundIndex >= tourData.length) return {round: -1 ,match: -1};
+    if (parseInt((document.getElementsByName("match")[0] as HTMLSelectElement).value) === undefined) return {round: roundIndex ,match: -1};
+    const matchIndex = parseInt((document.getElementsByName("match")[0] as HTMLSelectElement).value);
+    if (matchIndex < 0 || matchIndex >= tourData[roundIndex].matches.length) return {round: roundIndex ,match: -1};
+    return {round: roundIndex ,match: matchIndex};
 }
